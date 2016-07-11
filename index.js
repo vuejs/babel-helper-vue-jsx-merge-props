ec8274e9fa72e7f160b556d8dfa1051f242d7e83
Evan You <yyx990803@gmail.com>
2016-07-11 15:10:53 -0400

    use plain function

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-module.exports = require('babel-template')(`
-  function _mergeJSXProps (objs) {
-    var nestRE = /^(attrs|props|on|class|style|staticAttrs)$/
-    return objs.reduce(function (a, b) {
-      for (var key in b) {
-        if (a[key] && nestRE.test(key)) {
-          var aa = a[key]
-          var bb = b[key]
-          if (Array.isArray(aa)) {
-            a[key] = aa.concat(bb)
-          } else if (Array.isArray(bb)) {
-            a[key] = [aa].concat(bb)
-          } else {
-            for (var nestedKey in bb) {
-              aa[nestedKey] = bb[nestedKey]
-            }
-          }
+var nestRE = /^(attrs|props|on|class|style|staticAttrs)$/
+
+module.exports = function mergeJSXProps (objs) {
+  return objs.reduce(function (a, b) {
+    var aa, bb, key, nestedKey
+    for (key in b) {
+      aa = a[key]
+      bb = b[key]
+      if (aa && nestRE.test(key)) {
+        if (Array.isArray(aa)) {
+          a[key] = aa.concat(bb)
+        } else if (Array.isArray(bb)) {
+          a[key] = [aa].concat(bb)
         } else {
-          a[key] = b[key]
+          for (nestedKey in bb) {
+            aa[nestedKey] = bb[nestedKey]
+          }
         }
+      } else {
+        a[key] = b[key]
       }
-      return a
-    }, {})
-  }
-`)()
+    }
+    return a
+  }, {})
+}
